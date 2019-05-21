ee1d7f168df068f2d55945ad14738c86ed0cb173
Emily DeWitt <emilykdewitt@gmail.com>
2019-05-20 20:51:43 -0500

    got auth working

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,9 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import util from '../../helpers/util';
+
+import googleButton from './googleButton.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
 
 const authStringBuilder = () => {
   let domString = '';
-  domString += '<h3>Auth</h3>';
+  domString += '<button id="google-auth" class="btn btn-danger">';
+  domString += `<img src=${googleButton} />`;
+  domString += '</button>';
   util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 export default { authStringBuilder };
