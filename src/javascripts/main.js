e748da8e951eff59cff0ab781b2e60e19b07f7e8
Emily DeWitt <emilykdewitt@gmail.com>
2019-05-20 19:55:45 -0500

    lalala

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,14 @@
+import firebase from 'firebase/app';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
+
+import apiKeys from './helpers/apiKeys.json';
 
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
