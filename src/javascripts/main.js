477d44e8b9cdc07e0599b34a0a39c5b4fb366eb1
Emily DeWitt <emilykdewitt@gmail.com>
2019-05-20 21:25:44 -0500

    completely authenticating

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
+import MyNavbar from './components/MyNavbar/MyNavbar';
+import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
 
@@ -9,6 +11,8 @@ import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavbar.navbarEvents();
+  authData.checkLoginStatus();
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
